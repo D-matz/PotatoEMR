740d19ec38001f734c69f4a6fc371413036b4d53
D-matz <davidedmatz@gmail.com>
2025-04-06 15:41:43 -0500

    login

--- potato/Common/Login/CommonLogin_view.py
+++ potato/Common/Login/CommonLogin_view.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.urls import reverse
+from django.contrib.auth.decorators import login_not_required
 
+@login_not_required
 def login_page(request):
-    return render(request, 'CommonLogin_index.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                next_url = request.POST.get('next', '')
+                if next_url:
+                    return redirect(next_url)
+                return redirect(reverse('CommonHomePage_index'))
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'CommonLogin_index.html', {
+        'form': form,
+        'next': request.GET.get('next', '')
+    })
 
