18facdd786f97ed2276c61937c0df928d7ece1db
D-matz <davidedmatz@gmail.com>
2025-04-08 20:04:17 -0500

    may change binding

--- potato/FHIR_Resources/Appointment.py
+++ potato/FHIR_Resources/Appointment.py
@@ -1,70 +1,70 @@
-from django.db import models
-from ..FHIR_DataTypes.FHIR_generalpurpose import *
-from ..FHIR_DataTypes.FHIR_primitive import *
+# from django.db import models
+# from ..FHIR_DataTypes.FHIR_generalpurpose import *
+# from ..FHIR_DataTypes.FHIR_primitive import *
 
-class FHIR_Appointment(models.Model):
-    #Identifier ForeignKey to this, related_name=appointment_identifiers
-    class StatusChoices(models.TextChoices): PROPOSED = 'proposed', 'Proposed'; PENDING = 'pending', 'Pending'; BOOKED = 'booked', 'Booked'; ARRIVED = 'arrived', 'Arrived'; FULFILLED = 'fulfilled', 'Fulfilled'; CANCELLED = 'cancelled', 'Cancelled'; NOSHOW = 'noshow', 'No Show'; ENTERED_IN_ERROR = 'entered-in-error', 'Entered in Error'; CHECKED_IN = 'checked-in', 'Checked In'; WAITLIST = 'waitlist', 'Waitlist'
-    status = FHIR_primitive_CodeField(max_length=16, choices=StatusChoices.choices, null=True)
+# class FHIR_Appointment(models.Model):
+#     #Identifier ForeignKey to this, related_name=appointment_identifiers
+#     class StatusChoices(models.TextChoices): PROPOSED = 'proposed', 'Proposed'; PENDING = 'pending', 'Pending'; BOOKED = 'booked', 'Booked'; ARRIVED = 'arrived', 'Arrived'; FULFILLED = 'fulfilled', 'Fulfilled'; CANCELLED = 'cancelled', 'Cancelled'; NOSHOW = 'noshow', 'No Show'; ENTERED_IN_ERROR = 'entered-in-error', 'Entered in Error'; CHECKED_IN = 'checked-in', 'Checked In'; WAITLIST = 'waitlist', 'Waitlist'
+#     status = FHIR_primitive_CodeField(max_length=16, choices=StatusChoices.choices, null=True)
 
-    cancellationReason_cc = models.ManyToManyField(FHIR_GP_Coding, related_name="appointment_cancellationReason",
-                                            limit_choices_to={'binding__binding_rule': 'https://www.hl7.org/fhir/valueset-appointment-cancellation-reason.html'})
-    cancellationReason_cctext = FHIR_primitive_StringField(max_length=255, null=True, blank=True)
+#     cancellationReason_cc = models.ManyToManyField(FHIR_GP_Coding, related_name="appointment_cancellationReason",
+#                                             limit_choices_to={'binding__binding_rule': 'https://www.hl7.org/fhir/valueset-appointment-cancellation-reason.html'})
+#     cancellationReason_cctext = FHIR_primitive_StringField(max_length=255, null=True, blank=True)
 
-    #Class ForeignKey to this, related_name = appointment_classes
-    #ServiceCategory ForeignKey to this, related_name = appointment_ServiceCategorys
-    #Specialty ForeignKey to this, related_name = appointment_specialty
+#     #Class ForeignKey to this, related_name = appointment_classes
+#     #ServiceCategory ForeignKey to this, related_name = appointment_ServiceCategorys
+#     #Specialty ForeignKey to this, related_name = appointment_specialty
 
-    appointmentType_cc = models.ManyToManyField(FHIR_GP_Coding, related_name="appointment_appointmentType",
-                                            limit_choices_to={'binding__binding_rule': 'https://www.hl7.org/fhir/valueset-appointment-type.html'})
-    appointmentType_cctext = FHIR_primitive_StringField(max_length=255, null=True, blank=True)
+#     appointmentType_cc = models.ManyToManyField(FHIR_GP_Coding, related_name="appointment_appointmentType",
+#                                             limit_choices_to={'binding__binding_rule': 'https://www.hl7.org/fhir/valueset-appointment-type.html'})
+#     appointmentType_cctext = FHIR_primitive_StringField(max_length=255, null=True, blank=True)
 
-    #reason ForeignKey to this, related_name = appointment_reason
+#     #reason ForeignKey to this, related_name = appointment_reason
 
-    priority_cc = models.ManyToManyField(FHIR_GP_Coding, related_name="appointment_priority",
-                                            limit_choices_to={'binding__binding_rule': 'https://www.hl7.org/fhir/valueset-appointment-priority.html'})
-    priority_cctext = FHIR_primitive_StringField(max_length=255, null=True, blank=True)
+#     priority_cc = models.ManyToManyField(FHIR_GP_Coding, related_name="appointment_priority",
+#                                             limit_choices_to={'binding__binding_rule': 'https://www.hl7.org/fhir/valueset-appointment-priority.html'})
+#     priority_cctext = FHIR_primitive_StringField(max_length=255, null=True, blank=True)
 
-    description = FHIR_primitive_StringField(max_length=1024, null=True, blank=True)
+#     description = FHIR_primitive_StringField(max_length=1024, null=True, blank=True)
 
-    #replaces ForeignKey to this, related_name = appointment_replaces
-    #virtualService ForeignKey to this, related_name = appointment_virtualService
-    #supportingInformation ForeignKey to this, related_name = appointment_supportingInformation
+#     #replaces ForeignKey to this, related_name = appointment_replaces
+#     #virtualService ForeignKey to this, related_name = appointment_virtualService
+#     #supportingInformation ForeignKey to this, related_name = appointment_supportingInformation
     
-    previousAppointment = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name="previous_appointment")
-    originatingAppointment = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name="originating_appointment")
+#     previousAppointment = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name="previous_appointment")
+#     originatingAppointment = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name="originating_appointment")
 
-    start = FHIR_primitive_InstantField(max_length=64, null=True, blank=True)
-    end = FHIR_primitive_InstantField(max_length=64, null=True, blank=True)
-    minutesDuration = FHIR_primitive_PositiveIntField(null=True, blank=True)
+#     start = FHIR_primitive_InstantField(max_length=64, null=True, blank=True)
+#     end = FHIR_primitive_InstantField(max_length=64, null=True, blank=True)
+#     minutesDuration = FHIR_primitive_PositiveIntField(null=True, blank=True)
 
-    #requestedPeriod ForeignKey to this, related_name = appointment_requestedPeriod
-    #slot ForeignKey to this, related_name = appointment_slot
-    #account ForeignKey to this, related_name = appointment_account
+#     #requestedPeriod ForeignKey to this, related_name = appointment_requestedPeriod
+#     #slot ForeignKey to this, related_name = appointment_slot
+#     #account ForeignKey to this, related_name = appointment_account
 
-    created = FHIR_primitive_DateTimeField(null=True, blank=True)
-    cancellationDate = FHIR_primitive_DateTimeField(null=True, blank=True)
+#     created = FHIR_primitive_DateTimeField(null=True, blank=True)
+#     cancellationDate = FHIR_primitive_DateTimeField(null=True, blank=True)
 
-    #note ForeignKey to this, related_name = appointment_note
-    #patientInstruction ForeignKey to this, related_name = appointmnet_patientInstruction
-    #basedOn foreignKey to this, related_name = appointment_basdOn
+#     #note ForeignKey to this, related_name = appointment_note
+#     #patientInstruction ForeignKey to this, related_name = appointmnet_patientInstruction
+#     #basedOn foreignKey to this, related_name = appointment_basdOn
 
-    subject_patient = models.ForeignKey('FHIR_GP_Patient', null=True, on_delete=models.SET_NULL)
-    subject_group = models.ForeignKey('FHIR_GP_Patient', null=True, on_delete=models.SET_NULL)
+#     subject_patient = models.ForeignKey('FHIR_GP_Patient', null=True, on_delete=models.SET_NULL)
+#     subject_group = models.ForeignKey('FHIR_GP_Patient', null=True, on_delete=models.SET_NULL)
 
-    #participant foreign key to this
+#     #participant foreign key to this
 
-    recurrenceId = FHIR_primitive_PositiveIntField(null=True, blank=True)
-    occurrenceChanged = FHIR_primitive_BooleanField(null=True, blank=True)
+#     recurrenceId = FHIR_primitive_PositiveIntField(null=True, blank=True)
+#     occurrenceChanged = FHIR_primitive_BooleanField(null=True, blank=True)
 
-    #recurrenceTemplate foreign key to this
+#     #recurrenceTemplate foreign key to this
 
 
-class FHIR_Appointment_Identifier(FHIR_GP_Identifier):
-    appointment = models.ForeignKey(FHIR_Appointment, related_name="appointment_identifiers", on_delete=models.CASCADE)
+# class FHIR_Appointment_Identifier(FHIR_GP_Identifier):
+#     appointment = models.ForeignKey(FHIR_Appointment, related_name="appointment_identifiers", on_delete=models.CASCADE)
 
-class FHIR_Appointment_Class(models.Model):
-    appointment = models.ForeignKey(FHIR_Appointment, related_name="appointment_classes", on_delete=models.CASCADE)
+# class FHIR_Appointment_Class(models.Model):
+#     appointment = models.ForeignKey(FHIR_Appointment, related_name="appointment_classes", on_delete=models.CASCADE)
 
 
 # class FHIR_Appointment_Participant(models.Model):
